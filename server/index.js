4fd15d8919f9fdfe255395e42118bd51e953e9b9
Nitesh Chowdhary <thenitesh0112@gmail.com>
2023-06-05 21:12:31 +0530

    refactor

--- server/index.js
+++ server/index.js
@@ -2,22 +2,27 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+
 const app = express()
 const DATABASE = process.env.DATABASE
+const PORT = process.env.PORT || 4000
+
 app.use(cors())
 app.use(express.json())
 
 //db connection
-main().catch(err => console.log(err))
 
 async function main() {
     await mongoose.connect(DATABASE);
-    console.log("connected")
+    console.log("Connected to the database");
 }
+
+
+main().catch(err => console.log(err))
 
 const listRouter = require('./routes/list')
 app.use('/todo', listRouter.router)
 
-app.listen(process.env.PORT, () => {
+app.listen(PORT, () => {
     console.log("server started")
 })
