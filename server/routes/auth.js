4946d8978bd08a6bfa1a97d45b48fbb8c8394f0f
Nitesh Chowdhary <thenitesh0112@gmail.com>
2023-06-08 00:24:28 +0530

    created : sign in router

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
 const express = require("express")
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 const model = require("../model/user")
 const router = express.Router()
-
+const jwtToken = process.env.JWT_TOKEN
 const User = model.User
 
 router
@@ -39,5 +41,35 @@ router
                 console.log(err)
             })
     })
+    .post("/signin", (req, res) => {
+        const { email, password } = req.body
+        if (!email || !password) {
+            return res.json(422).json({ error: "Please fill the details" })
+        }
+        User.findOne({ email: email })
+            .then(savedUser => {
+                if (!savedUser) {
+                    return res.json(422).json({ error: "User doesn't exist" })
+                }
+                bcrypt.compare(password, savedUser.password)
+                    .then(doMatch => {
+                        if (doMatch) {
+                            const token = jwt.sign({ _id: savedUser._id }, jwtToken)
+                            res.json({ token })
+                        } else {
+                            return res.status(422).json({ error: "Invalid details" })
+                        }
+                    })
+                    .catch(err => {
+                        console.log(err)
+                    })
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    })
+
 
 exports.router = router
+
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDgwY2IwMTczYjIwNDBlMDQ2MTk1NWIiLCJpYXQiOjE2ODYxNjQwMDl9.qZkPaX_vIGz_A8Y3uDovMPxqcNNgt28g7CwSnfMYtzU
